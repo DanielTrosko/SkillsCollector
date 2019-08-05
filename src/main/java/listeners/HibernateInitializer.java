7df266a7c384a27a2653ce58f5e6f59c5f086382
Daniel Trosko <daniel.trosko@gmail.com>
2019-08-05 14:37:02 +0200

    init.sql

--- src/main/java/listeners/HibernateInitializer.java
+++ src/main/java/listeners/HibernateInitializer.java
@@ -8,10 +8,11 @@ import org.hibernate.service.ServiceRegistry;
 
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import javax.servlet.annotation.WebListener;
 import java.util.Properties;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-
+@WebListener
 public class HibernateInitializer implements ServletContextListener {
     @Override
     public void contextInitialized(ServletContextEvent sce) {
@@ -21,11 +22,11 @@ public class HibernateInitializer implements ServletContextListener {
 
             Properties hbnProperties = new Properties();
             hbnProperties.put(Environment.DRIVER, "org.mariadb.jdbc.Driver");
-            hbnProperties.put(Environment.URL, "jdbc:mariadb://51.38.133.27:3306/skills_collector");
+            hbnProperties.put(Environment.URL, "jdbc:mariadb://51.38.133.27:3306/skill?useSSL=false");
             // Nazwę użytkownika dostosuj do swojej instalacji MySQL
             hbnProperties.put(Environment.USER, "skill");
             // Hasło użytkownika dostosuj do swojej instalacji MySQL
-            hbnProperties.put(Environment.PASS, "skill123.");
+            hbnProperties.put(Environment.PASS, "jajo123.");
             hbnProperties.put(Environment.DIALECT, "org.hibernate.dialect.MariaDB103Dialect");
             hbnProperties.put(Environment.SHOW_SQL, "true");
             hbnProperties.put(Environment.FORMAT_SQL, "true");
@@ -33,7 +34,7 @@ public class HibernateInitializer implements ServletContextListener {
             // W przypadku gdy silnik Hibernate ma tworzyć schemat bazy danych, to poniżej
             // użyj opcji create-drop albo update
 //            hbnProperties.put(Environment.HBM2DDL_AUTO, "validate");
-            hbnProperties.put(Environment.HBM2DDL_AUTO, "validate");
+            hbnProperties.put(Environment.HBM2DDL_AUTO, "update");
             configuration.setProperties(hbnProperties);
 
             // Odkomentuj poniższe instrukcje po utworzeniu klas encji (kolejne zadania)
